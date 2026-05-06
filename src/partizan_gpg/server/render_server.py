78893cc49154cfb8b2270ba08eefb68a0c107e8a
nullchamber <131722143+nullchamber@users.noreply.github.com>
2026-05-05 19:49:53 -0500

    fixed

--- src/partizan_gpg/server/render_server.py
+++ src/partizan_gpg/server/render_server.py
@@ -2,12 +2,21 @@ import os
 from textual_serve.server import Server
 
 
-port = int(os.environ.get("PORT", 8000))
-server = Server(
-    "python partizan_gpg.tui.app",
-    port=port,
-    host="0.0.0.0"
-)
+# port = int(os.environ.get("PORT", 8000))
+# # server = Server(
+# #     "python partizan_gpg.tui.app",
+# #     port=port,
+# #     host="0.0.0.0",
+# # )
+# server = Server()
 
+# def main():
+#     server.serve()
 def main():
+    port =int(os.environ.get("PORT", 8000))
+    server = Server(
+        "python -m partizan_gpg",
+        port=port,
+        host="0.0.0.0"
+    )
     server.serve()
